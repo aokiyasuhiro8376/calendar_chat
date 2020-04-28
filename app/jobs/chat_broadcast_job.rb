a7216cd0e792c5099870c7d536479d4badab70ec
aokiyasuhiro8376 <56961562+aokiyasuhiro8376@users.noreply.github.com>
2020-04-29 07:34:31 +0900

    投稿未設定

--- app/jobs/chat_broadcast_job.rb
+++ app/jobs/chat_broadcast_job.rb
@@ -12,12 +12,12 @@ class DirectMessageBroadcastJob < ApplicationJob
   # queue_as :default
 
   def perform(chat)
-    ActionCable.server.broadcast "room_channel_#{chat.room_id}", chat: render_chats(chat)
+    ActionCable.server.broadcast "room_channel_#{chat.room_id}", chat: render_chat(chat)
   end
 
   private
 
-    def render_chats(chat)
-      ApplicationController.renderer.render partial: 'chats/chat', locals: { chat: chat }
-    end
+  def render_chat(chat)
+    ApplicationController.renderer.render partial: 'chats/chat', locals: { chat: chat }
+  end
 end
--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,8 +1,8 @@
 class Chat < ApplicationRecord
+  after_create_commit { ChatBroadcastJob.perform_later self }
+
   belongs_to :user
   belongs_to :room
 
   validates :message, presence: true
-
-  after_create_commit { ChatBroadcastJob.perform_later self }
 end
