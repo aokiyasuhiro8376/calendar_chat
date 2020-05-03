30441f86f38b97fdda89e1d8b2b057a7f9ce45ed
aokiyasuhiro8376 <56961562+aokiyasuhiro8376@users.noreply.github.com>
2020-05-03 22:43:24 +0900

    root検証

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,8 +1,9 @@
 class Chat < ApplicationRecord
-  after_create_commit { ChatBroadcastJob.perform_later self }
-
   belongs_to :user
   belongs_to :room
 
   validates :message, presence: true
+
+  #ブロードキャスト
+  after_create_commit { ChatBroadcastJob.perform_later self }
 end
